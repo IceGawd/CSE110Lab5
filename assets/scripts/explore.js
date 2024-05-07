210ad3dde2f9324529882c57cd647a3d47baa433
IceGawd <avighna.kukreja@gmail.com>
2024-05-06 22:42:58 -0700

    SpeechSynthesis part 1 done

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -2,6 +2,42 @@
 
 window.addEventListener('DOMContentLoaded', init);
 
+var voiceSelector = document.getElementById("voice-select");
+var talkButton = document.getElementsByTagName('button')[0];
+var face = document.getElementsByTagName('img')[0];
+var voices = [];
+
+function populateVoiceList() {
+  voices = window.speechSynthesis.getVoices();
+
+  voices.forEach(element => {
+    let option = document.createElement("option");
+    option.textContent = element.name;
+    voiceSelector.appendChild(option);
+  });
+}
+
+function voiceSpeak() {
+  let utterance = new SpeechSynthesisUtterance(document.getElementById("text-to-speak").value);
+  utterance.voice = voices[voiceSelector.selectedIndex - 1];
+  utterance.addEventListener("start", speaking);
+  utterance.addEventListener("end", smiling);
+  speechSynthesis.speak(utterance);
+}
+
+function speaking() {
+  face.src = "assets/images/smiling-open.png";
+}
+
+function smiling() {
+  face.src = "assets/images/smiling.png";
+}
+
 function init() {
-  // TODO
+  talkButton.onclick = voiceSpeak;
+
+}
+
+if (speechSynthesis.onvoiceschanged !== undefined) {
+  speechSynthesis.onvoiceschanged = populateVoiceList;
 }
